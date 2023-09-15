db4afe09d64a380853d75ef365ec84f56e0fc97b
Kelvin okumba <okumbakelvin@gmail.com>
2023-09-15 13:42:19 +0300

    navbar and footer

--- best-dishes/src/Components/Footer.js
+++ best-dishes/src/Components/Footer.js
@@ -1,11 +1,28 @@
 import React from "react";
 
 function Footer() {
-    return (
+  return (
+    <footer>
+      <div className="footer1">
+        <p>ABOUT US</p>
         <div>
-
+          <p>Our story</p>
+          <p></p>
         </div>
-    )
+      </div>
+
+      <div className="footer2">
+        <p>CONTACT US</p>
+        <p>
+          <a href="https://facebook.com">facebook</a>
+        </p>
+        <p>
+          {" "}
+          <a href="https://linkedin.com">linkedin</a>
+        </p>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
