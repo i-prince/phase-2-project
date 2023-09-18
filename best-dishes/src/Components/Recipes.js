8e3d0039ae4c4d8f954efb098f861c20829b098e
vchemu <valerine.chemutai@student.moringaschool.com>
2023-09-19 00:02:03 +0300

    Added post request

--- best-dishes/src/Components/Recipes.js
+++ best-dishes/src/Components/Recipes.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Addreview from './AddReview';
 
 function removeHtmlTags(input) {
   return input ? input.replace(/<[^>]*>?/gm, '') : '';
@@ -6,10 +7,19 @@ function removeHtmlTags(input) {
 
 function RecipeApp() {
   const [recipes, setRecipes] = useState([]);
+  const [recipeComments, setRecipeComments] = useState({});
+
+  const handleCommentSubmit = (comment, recipeId) => {
+    if (!recipeComments[recipeId]) {
+      recipeComments[recipeId] = [];
+    }
+    recipeComments[recipeId].push(comment);
+    setRecipeComments({ ...recipeComments });
+  };
 
   useEffect(() => {
     const apiUrl = 'https://api.spoonacular.com/recipes/random';
-    const apiKey = 'f064385afb96495d9195cb5b0e85f9c4'; 
+    const apiKey = 'f064385afb96495d9195cb5b0e85f9c4';
 
     const params = {
       limitLicense: true,
@@ -39,7 +49,18 @@ function RecipeApp() {
           <h3>{recipe.title}</h3>
           <img src={recipe.image} alt={recipe.title} />
           <p>{recipe.cuisines}</p>
-          <p>Instructions: {removeHtmlTags(recipe.instructions)}</p>
+          <p>Instructions: {recipe.instructions}</p>
+          <h3>Comments</h3>
+          <ul className="comment-list">
+            {recipeComments[recipe.id] &&
+              recipeComments[recipe.id].map((comment, index) => (
+                <li key={index}>
+                  {index + 1}. {comment}
+                </li>
+              ))}
+          </ul>
+
+          <Addreview onCommentSubmit={(comment) => handleCommentSubmit(comment, recipe.id)} />
         </div>
       ))}
     </div>
